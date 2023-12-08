52aae31fd5ebf221d98fa6e7f92e2d46a5174e76
federico <federico@pop-os.localdomain>
2023-12-08 12:29:42 +0100

    Add Makefile in CA/Theory

--- CA/Theory/make.sh
+++
@@ -1,3 +0,0 @@
-pandoc -N --variable "geometry=margin=1.1in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=11pt Computer_Architecture_Notes.md --pdf-engine=pdflatex --toc -o Computer_Architecture_Notes.pdf
-
-evince Computer_Architecture_Notes.pdf &
